61efdc5a0a9b6d9219959dc428a941984365af78
Thomas Keating <twkeating1@gmail.com>
2018-01-16 19:56:13 -0500

    Started Company Manager Project

--- cheat_sheet_python/python_cheat_sheet_practice.py
+++ cheat_sheet_python/python_cheat_sheet_practice.py
@@ -1,1 +1,47 @@
-print('Hello World')
+'''
+#Printing
+msg = "Hello World"
+print(msg)
+first_name = 'Thomas'
+last_name = 'Keating'
+
+bikes = [first_name, last_name]
+
+#Generating lists 1
+print(bikes[0] + bikes[1])
+bikes.append('monkey')
+print(bikes)
+
+numbers = []
+for x in range(1,12):
+  numbers.append(x**2)
+  
+print(numbers)
+
+#Generating Lists 2
+comprehended_numbers = [x**2 for x in range(1, 11)]
+print(comprehended_numbers)
+
+finishers = ['sam', 'bob', 'ada', 'bea']
+first_two = finishers[:2]
+print(finishers[1:3])
+
+#Tuples
+const = (1,2,3)
+print(const[1])
+
+outdoors = ['mountains', 'ocean', 'sky', 'animals']
+print("heater" in outdoors)
+print("comfort" not in outdoors)
+'''
+
+degrees_outside_Fahrenheit = 30
+
+if degrees_outside_Fahrenheit > 32:
+	print("It's only brisk!")
+else:
+	print("Ok it's a bit chilly, no?")
+
+
+
+
---
+++ company_manager/README.txt
@@ -0,0 +1,3 @@
+Inspired by this prompt at https://github.com/karan/Projects : 
+
+Company Manager - Create an hierarchy of classes - abstract class Employee and subclasses HourlyEmployee, SalariedEmployee, Manager and Executive. Every one's pay is calculated differently, research a bit about it. After you've established an employee hierarchy, create a Company class that allows you to manage the employees. You should be able to hire, fire and raise employees.
